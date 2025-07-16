6bc0555fce32ff0740622767871fecd4c713f9dc
Kirill S <skdamn@outlook.com>
2025-07-16 11:53:27 +0300

    fix: use native withdraw alm

--- src/components/modals/RemoveALMLiquidityModal/index.tsx
+++ src/components/modals/RemoveALMLiquidityModal/index.tsx
@@ -76,7 +76,7 @@ const RemoveALMLiquidityModal = ({ userVault, poolAddress }: RemoveALMLiquidityM
         const shareToWithdraw = BigNumber.from(lpShareToWithdraw.quotient.toString());
         try {
             let tx;
-            if (!useNative) {
+            if (useNative) {
                 tx = await withdrawNativeToken(
                     account,
                     shareToWithdraw,
